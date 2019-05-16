7de95836e71014a2fa66523ad8da1b5a0a5e7740
johnnyblame <iwishmynamewasdaniel@gmail.com>
2019-05-16 18:24:10 +0300

    minor changes

--- app/__init__.py
+++ app/__init__.py
@@ -4,6 +4,9 @@ from flask_migrate import Migrate
 from config import Config
 from flask_login import LoginManager
 from flask_babel import Babel
+import os
+import logging
+from logging.handlers import RotatingFileHandler
 
 app = Flask(__name__)
 app.config.from_object(Config)
